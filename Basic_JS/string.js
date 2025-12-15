b370d425e5c225b54d783c0eadb97f98331b4d69
suresh-ki <145967281+suresh-ki@users.noreply.github.com>
2025-12-15 12:24:31 +0000

    Add string split example

--- Basic_JS/string.js
+++ Basic_JS/string.js
@@ -1,1 +1,6 @@
-
+const myName ="Suresh is a good boy";
+//const lastName ="Nishad";
+//console.log(`My name is ${myName} hai, Aur mera sirname ${lastName} hai`)
+//onsole.log(myName.split(3))
+const newName = myName.split(" ");
+console.log(newName[3]);
